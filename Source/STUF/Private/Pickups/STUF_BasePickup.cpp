1488275ce4bd5a2000a95150480d2e9c624b7b20
DESKTOP-FTMGIA4\Intel <herelpeace@gmail.com>
2025-07-25 23:41:07 +0700

    7 раздел. 6. Вращение pickup объектов.FMathRandBool,FMathRandRange.

--- Source/STUF/Private/Pickups/STUF_BasePickup.cpp
+++ Source/STUF/Private/Pickups/STUF_BasePickup.cpp
@@ -24,12 +24,15 @@ void ASTUF_BasePickup::BeginPlay()
 	Super::BeginPlay();
 
 	check(CollisionComponent);
-	
+
+	GenerateRotationYaw();	
 }
 
 void ASTUF_BasePickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	AddActorLocalRotation(FRotator(0.0f,RotationYaw, 0.0f));
 
 }
 
@@ -72,6 +75,7 @@ void ASTUF_BasePickup::PickupWasTaken()
 
 void ASTUF_BasePickup::Respawn()
 {
+	GenerateRotationYaw();
 	CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
 
 	if (GetRootComponent())
@@ -79,4 +83,10 @@ void ASTUF_BasePickup::Respawn()
 		GetRootComponent()->SetVisibility(true, true);
 	}
 
+}
+
+void ASTUF_BasePickup::GenerateRotationYaw()
+{
+	const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
+	RotationYaw = FMath::RandRange(1.0f,2.0f)*Direction;
 }
